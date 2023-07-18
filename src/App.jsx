334838f6e71e058e642bd31779700c52a2001348
Nikolett Hevizi <nikihevizi@gmail.com>
2023-07-18 11:08:25 +0200

    ws

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 // use nextId() to generate a new unique ID when needed
-import nextId from "./generateId";
+//import nextId from "./generateId";
+import Dice from "./components/Dice";
+import DiceRoller from "./components/DiceRoller";
+import { useState } from "react";
 
 function App() {
+  const [roll, setRoll] = useState(0);
+
+  function handleRoll(num){
+    console.log(num);
+    setRoll(num)
+  }
+
   return (
     <>
       <p>Hello from the App!</p>
+      <Dice
+      number = { roll }/>
+      <DiceRoller
+      onRoll = { handleRoll }
+      />
     </>
   );
 }
